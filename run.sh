74e339f38510c1199ad99fbbb015d93d41c45d24
RiX <rix1337@users.noreply.github.com>
2017-12-11 14:10:34 +0100

    Initial attempt

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd /opt/plextogether
+if [[ -v AUTOJOIN ]]; then
+	if [ ! -f autojoinenabled ]; then
+		sed -i -e "s/autoJoin: false/autoJoin: true/g" /opt/plextogether/src/store.js
+		sed -i -e "s/autoJoinUrl: null/autoJoinUrl: '$DOMAIN'/g" /opt/plextogether/src/store.js
+		npm run build
+		touch autojoinenabled
+	fi
+fi
+npm run server & node webapp.js --url=https://$DOMAIN/ptweb
