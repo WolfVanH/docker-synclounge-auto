6107f1ea8bd3fb43627e8d1bd22c99bc08723c2f
Wolf Van Herreweghe <34643460+WolfVanH@users.noreply.github.com>
2019-05-27 02:39:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,18 +2,4 @@
 
 cd /opt/synclounge
 
-needRebuild=false
-if [ $webroot != "" ]; then
-  needRebuild=true
-fi
-if [ $autoJoin == "true" ]; then
-  needRebuild=true
-fi
-if [ $needRebuild == "true" ]; then
-  echo 'Rebuilding Webapp for custom options support. This can take a minute or two.'
-  npm install
-  npm run build
-else
-  echo 'Not rebuilding webapp'
-fi
 export accessUrl=https://$DOMAIN; npm run server & node webapp.js
